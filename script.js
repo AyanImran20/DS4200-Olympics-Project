1186f317d42bdac05c984fca0445ad77f687150a
Ayan Imran <imran.aya@northeastern.edu>
2025-11-22 12:38:07 -0500

    Initial website build

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+// This is where we'll add interactivity (D3, Altair embeds, etc.)
+
+console.log("Olympics visualization site loaded.");
+
+// TODO (later steps):
+// 1. Load processed data (e.g., medals_by_country.json).
+// 2. Build world medal map inside #world-map using D3.
+// 3. Create bar chart race in #bar-race.
+// 4. Add gender participation dashboard in #gender-dashboard.
